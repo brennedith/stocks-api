aab571f9d6a49c558b1e31f0f118eb32289db29d
Brennedith Garcia <brennedith@pm.me>
2023-10-30 10:32:35 -0600

    feat: add Dockerfile

--- app/base/settings.py
+++ app/base/settings.py
@@ -23,9 +23,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-!#!2trxbrw+#^(!$$+^oo!gj@3bvbo=n!#v@my%5a)5$6_9r18'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
